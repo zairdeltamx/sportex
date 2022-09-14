e4c73df972155b678847c653afe24a3832ed9347
Juan Hinojo <jchinojob7@gmail.com>
2022-09-14 18:31:57 -0500

    Use webpack for stylesheets entry

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,8 @@ import * as ActiveStorage from "@rails/activestorage";
 import Rails from "@rails/ujs";
 import Turbolinks from "turbolinks";
 
+import '../stylesheets/application';
+
 Rails.start();
 Turbolinks.start();
 ActiveStorage.start();
