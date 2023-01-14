fd8a710554cac69fe0a8a40d934d1babadeca570
Juan Hinojo <jchinojob7@gmail.com>
2023-01-14 09:49:58 -0600

    Remove css from manifest.js

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,4 @@
 //= link_tree ../images
-//= link_tree ../../javascrip/stylesheets .csst
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 //= link grape_swagger_rails/application.css
