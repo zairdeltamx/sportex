b4afce6d4b9b8919b17d5ebe60606c6bf8ef808e
zairdelta <zairortiz99@gmail.com>
2023-01-27 15:45:59 -0600

    Feature: migration email added

--- db/migrate/20220908144910_create_users.rb
+++ db/migrate/20220908144910_create_users.rb
@@ -4,7 +4,6 @@ class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       t.string :username
-      t.string :email
       t.string :eth_address
       t.string :eth_nonce
       t.timestamps
