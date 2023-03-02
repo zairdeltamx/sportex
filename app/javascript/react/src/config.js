5d596b5a196e76c0400738f872a22c2718785d12
Juan Hinojo <jchinojob7@gmail.com>
2023-03-02 11:04:24 -0600

    Remove esbuild config build

--- app/javascript/react/src/config.js
+++ app/javascript/react/src/config.js
@@ -4,4 +4,4 @@ export const nftmarketaddress = "0xcB2788f8D46C1f6f2C22D3A6b455D2E62EFcC69a";
 
 // export const getApiUrl = (path) => `http://localhost:3000/api/v1/${path}`;
 export const getApiUrl = (path) =>
-  `https://sportex.herokuapp.com/api/v1/${path}`;
+  `https://sportex-staging.herokuapp.com/api/v1/${path}`;
