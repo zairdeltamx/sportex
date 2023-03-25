75913a8d9f8d4eeae4912315a74eabd736a491d6
Juan Hinojo <jchinojob7@gmail.com>
2023-03-25 12:08:02 -0500

    Attach updated schema

--- db/migrate/20230127215032_add_email_to_users.rb
+++ db/migrate/20230127215032_add_email_to_users.rb
@@ -1,5 +1,7 @@
 class AddEmailToUsers < ActiveRecord::Migration[7.0]
   def change
+    return if column_exists?(:users, :email)
+
     add_column :users, :email, :string
   end
 end
--- db/migrate/20230302163153_add_remember_created_at_to_users.rb
+++ db/migrate/20230302163153_add_remember_created_at_to_users.rb
@@ -1,5 +1,7 @@
 class AddRememberCreatedAtToUsers < ActiveRecord::Migration[7.0]
   def change
+    return if column_exists?(:users, :remember_created_at)
+
     add_column :users, :remember_created_at, :datetime
   end
 end
