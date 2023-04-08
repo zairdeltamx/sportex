5a5b5a755c22b37c6e49ba2da9e2c71d42fe9535
Juan Hinojo <jchinojob7@gmail.com>
2023-04-08 16:36:21 -0400

    Attach specs for nfts graphql queries and NFT marketplace contract

--- app/javascript/react/hardhat/test/marketplace-test.js
+++ app/javascript/react/hardhat/test/marketplace-test.js
@@ -1,39 +1,103 @@
-// const { ethers } = require("hardhat");
+const { ethers } = require("hardhat");
+const { expect } = require("chai");
 
-// describe("NFTMarket", function () {
-//   it("Should create a new Sale", async function () {
-//     const Market = await ethers.getContractFactory("NFTMarketplace");
+describe("NFTMarket", function () {
+  it("Should create a new Sale", async function () {
+    const Market = await ethers.getContractFactory("NFTMarketplace");
 
-//     const market = await Market.deploy();
-//     await market.deployed();
-//     const marketAddress = market.address;
+    const market = await Market.deploy();
+    await market.deployed();
+    const marketAddress = market.address;
 
-//     const NFT = await ethers.getContractFactory("NFT");
-//     const nft = await NFT.deploy(marketAddress);
-//     await nft.deployed();
-//     const nftContractAddres = nft.address;
+    const NFT = await ethers.getContractFactory("NFT");
+    const nft = await NFT.deploy(marketAddress);
+    await nft.deployed();
+    const nftContractAddres = nft.address;
 
-//     let listingPrice = await market.getListingPrice();
-//     listingPrice = listingPrice.toString();
+    const FakeNFTContract = await ethers.getContractFactory("NFT");
+    const fakeNft = await FakeNFTContract.deploy(marketAddress);
+    await fakeNft.deployed();
+    const fakenftContractAddres = fakeNft.address;
 
-//     const auctionPrice = ethers.utils.parseUnits("5", "ether");
+    let listingPrice = await market.getListingPrice();
+    listingPrice = listingPrice.toString();
 
-//     await nft.createToken("https://axtrading.io/1", "{test:one}");
-//     await nft.createToken("https://axtrading.io/2", "{test:two}");
+    const auctionPrice = ethers.utils.parseUnits("5", "ether");
 
-//     await market.createMarketItem(nftContractAddres, 1, auctionPrice, {
-//       value: listingPrice,
-//     });
-//     await market.createMarketItem(nftContractAddres, 2, auctionPrice, {
-//       value: listingPrice,
-//     });
+    // Should allow to list a new token
+    await nft.createToken("https://axtrading.io/1", "{test:one}");
+    await nft.createToken("https://axtrading.io/2", "{test:two}");
 
-//     const [_, buyerAddress] = await ethers.getSigners();
+    await market.listMarketItem(nftContractAddres, 1, auctionPrice, {
+      value: listingPrice,
+    });
+    await market.listMarketItem(nftContractAddres, 2, auctionPrice, {
+      value: listingPrice,
+    });
 
-//     await market
-//       .connect(buyerAddress)
-//       .createMarketSale(nftContractAddres, 1, { value: auctionPrice });
+    const [_, buyerAddress] = await ethers.getSigners();
 
-//     const items = await market.fetchMarketItems();
-//   });
-// });
+    const beforePurchasedItems = await market.fetchMarketItems();
+
+    expect(beforePurchasedItems.length).to.equal(2);
+
+    // Should allow to make a purchase
+    await market
+      .connect(buyerAddress)
+      .purchaseItem(nftContractAddres, 1, { value: auctionPrice });
+
+    const items = await market.fetchMarketItems();
+
+    expect(items.length).to.equal(1);
+
+    // should Allow to resell a token
+    const newPrice = ethers.utils.parseUnits("10", "ether");
+
+    await nft.connect(buyerAddress).approve(marketAddress, 1);
+
+    await market
+      .connect(buyerAddress)
+      .resellToken(nftContractAddres, 1, newPrice, {
+        value: listingPrice,
+      });
+
+    const resellItems = await market.fetchMarketItems();
+
+    const newPrice2 = ethers.utils.parseUnits("20", "ether");
+
+    expect(resellItems.length).to.equal(2);
+    expect(resellItems[0].price).to.equal(newPrice);
+
+    // Should not allow to list a token from another contract
+    await expect(
+      market
+        .connect(buyerAddress)
+        .resellToken(fakenftContractAddres, 1, newPrice, {
+          value: listingPrice,
+        })
+    ).to.be.revertedWith("Only item owner can perform this operation");
+
+    const newResellItems = await market.fetchMarketItems();
+
+    expect(newResellItems.length).to.equal(2);
+
+    // allows to change price of a token
+    await market
+      .connect(buyerAddress)
+      .changePrice(1, newPrice2);
+
+    const newResellItemsChange = await market.fetchMarketItems();
+
+    expect(newResellItemsChange.length).to.equal(2);
+    expect(newResellItemsChange[0].price).to.equal(newPrice2);
+
+    // allows to deList a token from the market
+    await market
+      .connect(buyerAddress)
+      .delistNFT(nftContractAddres, 1);
+
+    const updatedItems = await market.fetchMarketItems();
+
+    expect(updatedItems.length).to.equal(1);
+  });
+});
---
+++ spec/graphql/queries/nfts_spec.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe SportexSchema, elasticsearch: true, turnip_vcr: true do
+  let(:context) { {} }
+  let(:variables) { {} }
+
+  let(:result) do
+    SportexSchema.execute(
+      query_string,
+      context:,
+      variables:
+    )
+  end
+
+  describe 'querying for states index' do
+    let!(:nfts) { create_list(:nft, 3) }
+
+    let(:query_string) do
+      %|
+      query {
+        nfts {
+          collection {
+            id
+            name
+            price
+            defense
+            attack
+            teamName
+            owner
+            image
+            description
+            tokenId
+            strength
+            meta
+          }
+          metadata {
+            totalPages
+            totalCount
+          }
+        }
+      }
+      |
+    end
+
+    subject { result['data']['nfts'] }
+
+    it 'returns a list of states with a cities count' do
+      expect(subject['collection'].size).to eq Nft.count
+    end
+  end
+end
