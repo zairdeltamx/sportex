92bfe02c9be1ea25894140d36925e9e01e745858
Juan Hinojo <jchinojob7@gmail.com>
2023-04-13 23:57:40 -0500

    Attach web3 errors try

--- app/javascript/controllers/js/ethUtils.js
+++ app/javascript/controllers/js/ethUtils.js
@@ -20,18 +20,23 @@ export async function currentChainIsValid() {
 }
 
 export async function switchChain() {
-    alert("You are on the wrong network. Switching to the correct network");
-    const provider = await detectEthereumProvider();
-    const web3 = new Web3('https://rpc.v3.testnet.pulsechain.com/');
 
     alert("Switching to the correct network");
     try {
+      alert("You are on the wrong network. Switching to the correct network");
+      const provider = await detectEthereumProvider();
+      const web3 = new Web3('https://rpc.v3.testnet.pulsechain.com/');
+
       await provider.request({
         method: 'wallet_switchEthereumChain',
         params: [{ chainId: web3.utils.toHex(chainId) }],
       });
     } catch (error) {
       alert(error)
+
+      const provider = await detectEthereumProvider();
+      const web3 = new Web3('https://rpc.v3.testnet.pulsechain.com/');
+
       if (error.code === 4902) {
         await provider.request({
             method: "wallet_addEthereumChain",
