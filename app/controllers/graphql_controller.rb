113e8f9e300e2fe0e5573fdef34b28e77ed69a82
Juan Hinojo <jchinojob7@gmail.com>
2023-06-29 00:03:58 -0500

    remove authentication requirement for graphql_controller

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -4,7 +4,6 @@ class GraphqlController < ApplicationController
   # If accessing from outside this domain, nullify the session
   # This allows for outside API access while preventing CSRF attacks,
   # but you'll have to authenticate your user separately
-  before_action :authenticate_user!
   skip_before_action :verify_authenticity_token
 
   def execute
