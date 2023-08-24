67cf081a62dff0b809d3c0823d094d5f0abb02a6
zairdelta <zairortiz99@gmail.com>
2023-08-24 17:18:21 -0600

    Features: changed nfts.rb

--- app/graphql/queries/nfts.rb
+++ app/graphql/queries/nfts.rb
@@ -17,33 +17,40 @@ module Queries
         argument :order, String, required: false
         argument :orderBy, String, required: false
         argument :seller, String, required: false
-        argument :teamName, String, required: false 
-        argument :page, GraphQL::Types::Int, required: false, default_value: 1
-        argument :per_page, GraphQL::Types::Int, required: false, default_value: 10
+        argument :teamName, String, required: false
+        argument :current_page, GraphQL::Types::Int, required: false
+        argument :per_page, GraphQL::Types::Int, required: false, default_value: 4
       end
 
       field :getNFT, Types::NftType, null: false,
-                                  description: "Get one Nft by id" do
+                                     description: "Get one Nft by id" do
         argument :id, Integer, required: true
       end
       field :teams, [String], null: false,
                               description: "Obtener todos los equipos únicos de NFTs disponibles"
     end
-
-    def allNFTs(name: nil, order: nil, orderBy: nil, seller: nil, teamName: nil, status: nil, page:, per_page:)
-      nfts = Nft.with_status(:available).ransack(
-        name_cont: name,
-        seller_eq: seller,
-        teamName_cont: teamName,
-        status_eq: status,
-        s: "#{orderBy} #{order}"
-      ).result.non_presale.page(page).per(per_page)
-
+    def allNFTs(name: nil, order: nil, orderBy: nil, seller: nil, teamName: nil, status: nil, current_page: nil, per_page: nil)
+      puts "---------------------------------------------------------"
+      puts current_page
+      puts "---------------------------------------------------------"
+      
+      ransack_args = {}
+      ransack_args[:name_cont] = name.downcase if name.present?
+      ransack_args[:seller_eq] = seller.downcase if seller.present?
+      ransack_args[:teamName_cont] = teamName.downcase if teamName.present?
+      ransack_args[:s] = "#{orderBy} #{order}" if orderBy.present? && order.present?
+    
+      nfts = Nft.with_status(:available).ransack(ransack_args).result.non_presale.page(current_page).per(per_page)
+    
       {
         nfts: nfts,
         totalPages: nfts.total_pages,
+        # currentPage: current_page,
       }
     end
+    
+    
+    
 
     def getNFT(id:)
       Nft.find_by(id:)
